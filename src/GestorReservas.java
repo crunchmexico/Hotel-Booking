b6a84c30bc71535e5b7d10015059268254955d9e
Javier Ivan Ramirez Diaz <xaviercancun@gmail.com>
2025-05-12 00:56:53 -0500

    first commit On branch main Initial commit 	new file:   .vscode/settings.json 	new file:   README.md 	new file:   bin/App.class 	new file:   bin/GestorReservas.class 	new file:   src/App.java

---
+++ src/GestorReservas.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestorReservas {
+    private List<Reserva> listaDeReservas;
+
+    public GestorReservas() {
+        this.listaDeReservas = new ArrayList<>();
+    }
+
+    public void agregarReserva(Reserva nuevaReserva) {
+        this.listaDeReservas.add(nuevaReserva);
+    }
+
+    public void mostrarReservas() {
+        for (Reserva reserva : listaDeReservas) {
+            System.out.println("Reserva: " + reserva.getNumeroReserva());
+            System.out.println("Nombre del Huesped: " + reserva.getNombreHuesped() + " " + reserva.getApellidoHuesped());
+        }
+    }
+
+    public void eliminarReserva(int numeroReserva) {
+        for (int i = 0; i < listaDeReservas.size(); i++) {
+            if (listaDeReservas.get(i).getNumeroReserva() == numeroReserva) {
+                
+            }
+        }
+    }
+}
